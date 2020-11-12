688c37544af144ad9acc3af11fdcd94a2af18b63
TomGlad228 <molochkov2017@gmail.com>
2020-11-12 21:41:42 +0300

    Добавьте файлы проекта.

---
+++ 25.4/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+	int count = 3577000;
+	bool a;
+	for (int i = 2; i <= 3577000; i++) {
+		a = true;
+		for (int d = 2; d <= i - 1; d++) {
+			if (i % d == 0) {
+				a = false;
+				break;
+			}
+		}
+		if (a) {
+			count--;
+		}
+	}
+	std::cout << count;
+}
